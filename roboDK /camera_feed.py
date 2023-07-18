fbac24c276b912e5ca7826e02bc4d66d6bb27e67
Akshit0601 <akshit.shishodia@icloud.com>
2023-07-18 13:34:05 +0530

    updated;

--- roboDK /camera_feed.py
+++ roboDK /camera_feed.py
@@ -6,7 +6,7 @@ import numpy as np
 import cv2 as cv
 import os
 
-# import tensorflow
+import tensorflow
 cnt = 0
 base_path = '/Users/akshitshishodia/tracker/DATASET_/DATASET_'
 
@@ -17,7 +17,7 @@ def convert_frame(frame):
     x = np.expand_dims(x,axis=0)
     return x
 
-# model = tensorflow.keras.models.load_model('/Users/akshitshishodia/tracker/files/model_2.h5')
+model = tensorflow.keras.models.load_model('/Users/akshitshishodia/tracker/files/model.h5')
 
 CAM_NAME = 'Camera 1'
 CAM_PARAMS = 'SIZE=640x480' 
@@ -46,23 +46,23 @@ while cam_item.setParam('isOpen') == '1':
     x = convert_frame(img_socket) 
 
 
-    # prediction = model.predict(x)
-    # x1,y1,x2,y2 = prediction[0]
-    # h = img_socket.shape[1]
-    # w = img_socket.shape[0]
-    # x1 = int(x1*h)
-    # y1 = int(y1*w)
+    prediction = model.predict(x)
+    x1,y1,x2,y2 = prediction[0]
+    h = img_socket.shape[1]
+    w = img_socket.shape[0]
+    x1 = int(x1*h)
+    y1 = int(y1*w)
 
-    # x2 = int(x2*h)
-    # y2 = int(y2*w)
+    x2 = int(x2*h)
+    y2 = int(y2*w)
   
-    # img_socket = cv.rectangle(img_socket,(x1,y1),(x2,y2),(0,255,0),thickness=3)
-    if cnt%2 == 0 :
-        name = str(cnt) + '.png'
-        path  = os.path.join(base_path,name) 
-        cv.imwrite(path,img_socket)
-    cnt+=1
-
+    img_socket = cv.rectangle(img_socket,(x1,y1),(x2,y2),(0,255,0),thickness=3)
+    # if cnt%2 == 0 :
+    #     name = str(cnt) + '.png'
+    #     path  = os.path.join(base_path,name) 
+    #     cv.imwrite(path,img_socket)
+    # cnt+=1
+    cv.imshow('detect',img_socket)
     key = cv.waitKey(1)
     if key == 27:
         break 
