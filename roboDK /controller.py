0194c872e999795e23810fda4f707f35543910ee
Akshit0601 <akshit.shishodia@icloud.com>
2023-07-20 18:43:09 +0530

    pick only 3,4

--- roboDK /controller.py
+++ roboDK /controller.py
@@ -21,7 +21,7 @@ p6 = RDK.Item('pos6')
 p7 = RDK.Item('pos7')
 p8 = RDK.Item('pos8')
 p9 = RDK.Item('pos9')
-inventory_targets = [p1,p2,p3,p4,p5,p6,p7,p8,p9]
+inventory_targets = [p5,p6]
 
 gantry = RDK.Item("Gantry")
 Target_3 = RDK.Item("zero")
@@ -70,15 +70,15 @@ def shift_inventory():
         inventory.MoveJ(i)
         logging.info(inventory.Joints())
         print("Current Inventory Joint Pose : ",inventory.Joints())
-        sleep(2)
+        # sleep(1)
         pick_lower()
-        sleep(2)
-        pick_upper()
+        # sleep(1)
+        # pick_upper()
 
 
 while(1):
-    gantry.setSpeedJoints(2)
-    inventory.setSpeedJoints(2)
+    gantry.setSpeedJoints(10)
+    inventory.setSpeedJoints(10)
     shift_inventory()
 
 
