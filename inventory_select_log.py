f496760b324eecbd23d08caabe9bd0fdb22e2c48
Akshit0601 <akshit.shishodia@icloud.com>
2023-07-25 11:08:03 +0530

    updated;

--- inventory_select_log.py
+++ inventory_select_log.py
@@ -1,21 +1,16 @@
 import logging
-
 logging.basicConfig(filename='main.log',filemode='w',format='%(asctime)s - %(message)s', level=logging.INFO)
-
 sim_log = list()
 
-# 
 with open('/Users/akshitshishodia/tracker/roboDK /simulation.log',"r") as f:
     for line in f :
         try:
             sim_log.append(line.split()[4])
         except:
             break
-
 sim_log = list(map(lambda x : float(x),sim_log))
 sim_log = list(map(lambda x : (x-45)/35 +1,sim_log))
 sim_log = list(map(lambda x : str(x),sim_log))
-
 iou_log = list()
 with open('/Users/akshitshishodia/tracker/iou.log','r') as f:
     for line in f:
@@ -23,7 +18,6 @@ with open('/Users/akshitshishodia/tracker/iou.log','r') as f:
             iou_log.append(line.split()[4])
         except:
             pass
-
 for idx,i in enumerate(iou_log):
     if i == 'neither':
         continue
